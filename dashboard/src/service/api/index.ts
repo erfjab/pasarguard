4481c8bfd9749b2b6c70f47c73833e9b12c42828
Mohammad <immohammad20000@protonmail.com>
2025-10-27 23:32:42 +0330

    bump to v1.3.1

--- dashboard/src/service/api/index.ts
+++ dashboard/src/service/api/index.ts
@@ -3,7 +3,7 @@
  * Do not edit manually.
  * PasarGuardAPI
  * Unified GUI Censorship Resistant Solution
- * OpenAPI spec version: 1.3.0
+ * OpenAPI spec version: 1.3.1
  */
 import { useMutation, useQuery } from '@tanstack/react-query'
 import type {
