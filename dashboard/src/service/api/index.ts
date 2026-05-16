e597e9010760d22f5b08476af3efd5a9836e0faf
x0sina <bulletsina77@gmail.com>
2026-05-16 21:01:42 +0330

    bump to v4.0.1

--- dashboard/src/service/api/index.ts
+++ dashboard/src/service/api/index.ts
@@ -3,7 +3,7 @@
  * Do not edit manually.
  * PasarGuardAPI
  * Unified GUI Censorship Resistant Solution
- * OpenAPI spec version: 4.0.0
+ * OpenAPI spec version: 4.0.1
  */
 import { useMutation, useQuery } from '@tanstack/react-query'
 import type {
