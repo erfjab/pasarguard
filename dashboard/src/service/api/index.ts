0b0ddaa9a259692004ffcf904cafec7a0daf8467
M03ED <50927468+M03ED@users.noreply.github.com>
2026-05-17 13:56:18 +0330

    bump to v4.0.2

--- dashboard/src/service/api/index.ts
+++ dashboard/src/service/api/index.ts
@@ -3,7 +3,7 @@
  * Do not edit manually.
  * PasarGuardAPI
  * Unified GUI Censorship Resistant Solution
- * OpenAPI spec version: 4.0.1
+ * OpenAPI spec version: 4.0.2
  */
 import { useMutation, useQuery } from '@tanstack/react-query'
 import type {
